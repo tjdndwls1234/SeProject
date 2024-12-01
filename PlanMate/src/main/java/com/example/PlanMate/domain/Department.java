293b4f7067a81a0a6e80ab1688a6b1830ffc9320
IA7420 <alstjdmstj1@uos.ac.kr>
2024-12-02 01:42:37 +0900

    Delete PlanMate directory

--- PlanMate/src/main/java/com/example/PlanMate/domain/Department.java
+++
@@ -1,7 +0,0 @@
-package com.example.PlanMate.domain;
-
-public class Department {
-    private int deparmentCode;
-    private String departmentName;
-    private String departmentCall;
-}
--- PlanMate/src/main/java/com/example/PlanMate/dto/StudentScheduleDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.PlanMate.dto;
-
-public class StudentScheduleDto {
-
-}
