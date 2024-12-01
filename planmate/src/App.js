b8235640e886ca4e7eaae8b0e4223d2ca95c10cb
JaeukRyu <martin0106@naver.com>
2024-12-01 19:35:26 +0900

    성적표 페이지 구현

--- planmate/src/App.js
+++ planmate/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Course from './pages/Course';
+import Transcript from './pages/Transcript';
+import Prerequisite from './pages/Prerequisite';
 import Index from './pages/Index';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/transcript" element={<Transcript />} />
+        <Route path="/prerequisite" element={<Prerequisite />} />
       </Routes>
     </Router>
   );
