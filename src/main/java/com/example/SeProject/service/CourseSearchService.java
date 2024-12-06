120f41475cd4623ee7914fde7050260c6d2826aa
MinseoKim <alstjdmstj1@office.uos.ac.kr>
2024-12-06 15:33:40 +0900

    버그 수정

--- src/main/java/com/example/SeProject/service/CourseSearchService.java
+++ src/main/java/com/example/SeProject/service/CourseSearchService.java
@@ -10,7 +10,6 @@ import com.example.SeProject.mapper.StudentScheduleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -31,14 +30,13 @@ public class CourseSearchService {
     public List<CourseDto> searchCourseList(CourseSearchCriteria criteria){
         List<CourseDto> courseList = courseSearchMapper.searchCourseList(criteria);
         if (courseList.isEmpty()) return courseList;
-        ArrayList<String> deleteCourseCode = new ArrayList<>();
-        ArrayList<Integer> deleteIndex = new ArrayList<>();
+
         //재수강 가능 과목 필터링
         if (Objects.equals(criteria.getIsRetakeableCourse(), "Y")) {
             List<String> retakeableCourseList = this.studentReportMapper.retakeableCourseListSearch(criteria);
 
             if (!retakeableCourseList.isEmpty()) {
-                // 재수강 가능한 과목을 필터링하여 제거
+
                 courseList.removeIf(course -> retakeableCourseList.contains(course.getCourseCode()));
             }
         }
@@ -49,10 +47,10 @@ public class CourseSearchService {
             courseList.removeIf(course -> {
                 for (StudentScheduleDto courseOnTimetable : courseListOnTimetable) {
                     if (isOverlapping(courseOnTimetable, course)) {
-                        return true; // 겹치는 과목은 제거
+                        return true;
                     }
                 }
-                return false; // 겹치지 않으면 남김
+                return false;
             });
         }
         return courseList;
