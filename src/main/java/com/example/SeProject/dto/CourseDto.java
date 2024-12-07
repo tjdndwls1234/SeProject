543df206754bfa62a43d0e04137a6d685144fa47
IA7420 <alstjdmstj1@naver.com>
2024-12-07 20:32:31 +0900

    안쓰는 Dto 삭제

--- src/main/java/com/example/SeProject/dto/CourseDto.java
+++ src/main/java/com/example/SeProject/dto/CourseDto.java
@@ -18,24 +18,4 @@ public class CourseDto {
     private String courseStartTime;
     private String courseEndTime;
     private String classroom;
-
-    public CourseDto() {
-    }
-
-    public CourseDto(String courseCode, String courseName, String departmentName, char grade, int credit,
-                     String courseDevision, String professorName, String courseDay, String totalCapacity,
-                     String courseStartTime, String courseEndTime, String classroom) {
-        this.courseCode = courseCode;
-        this.courseName = courseName;
-        this.departmentName = departmentName;
-        this.grade = grade;
-        this.credit = credit;
-        this.courseDevision = courseDevision;
-        this.professorName = professorName;
-        this.courseDay = courseDay;
-        this.totalCapacity = totalCapacity;
-        this.courseStartTime = courseStartTime;
-        this.courseEndTime = courseEndTime;
-        this.classroom = classroom;
-    }
 }
