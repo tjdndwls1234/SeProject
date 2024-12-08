b6f4fc4e6bad6449f3894fc1eec30c88c006c4cc
MinseoKim <alstjdmstj1@office.uos.ac.kr>
2024-12-08 15:52:34 +0900

    PrePost 구현

---
+++ src/main/java/com/example/SeProject/dto/PrePostCourseDto.java
@@ -0,0 +1,19 @@
+package com.example.SeProject.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PrePostCourseDto {
+    private String preCourseName;
+    private String preCourseDevision;
+    private String preCourseGrade;
+    private String preCourseTerm;
+    private String preCourseCredit;
+    private String postCourseName;
+    private String postCourseDevision;
+    private String postCourseGrade;
+    private String postCourseTerm;
+    private String postCourseCredit;
+}
